760efb3c16c449462c30b2d896fb90a1ae49f313
Ruwan Ranaweera <rwnranaweera@gmail.com>
2022-06-01 09:40:08 +0530

    react sample app

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Tweet from './Tweet';
+import './App.css';
+function App(){
+
+
+  return (
+    <div className="app">
+      <h1>Hello React</h1>
+      <Tweet/>
+      <Tweet/>
+      <Tweet/>
+
+    </div>
+  );
+}
+
+export default App;
