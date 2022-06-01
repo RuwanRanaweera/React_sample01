1a2266eedb49525e9e554772121454e0b22aa37d
Ruwan Ranaweera <rwnranaweera@gmail.com>
2022-06-01 17:05:30 +0530

    functions

--- src/App.js
+++ src/App.js
@@ -3,26 +3,34 @@ import Tweet from './Tweet';
 import './App.css';
 function App(){
 
- const [isRed, setRed] =  useState(false);
- const [count, setCount] = useState(0);
+const [users,setUsers] = useState([
+  {name: 'Ruwan', message:'Hello How are you'},
+  {name: 'Ranaweera' , message:'I am fine'}
+]);
+//  const [isRed, setRed] =  useState(false);
+//  const [count, setCount] = useState(0);
 
- const [user, setUser ] = useState({
-   name: 'Ruwan',
-   age: '27',
-   posts:['my first post', 'my lovely summer']
+//  const [user, setUser ] = useState({
+//    name: 'Ruwan',
+//    age: '27',
+//    posts:['my first post', 'my lovely summer']
 
- });
+//  });
 
- const increment = ()=>{
-   setCount(count + 1);
-   setRed(!isRed);
- };
+//  const increment = ()=>{
+//    setCount(count + 1);
+//    setRed(!isRed);
+//  };
 
   return (
     <div className="app">
-      <h1 className={isRed ? 'red' : ''}>Change my color !!!</h1>
+      {users.map(user=>
+        (
+        <Tweet name={user.name} message={user.message}/>
+        ))}
+      {/* <h1 className={isRed ? 'red' : ''}>Change my color !!!</h1>
     <button onClick={increment}>Increment</button>
-    <h1>{count}</h1>
+    <h1>{count}</h1> */}
 
     </div>
   );
